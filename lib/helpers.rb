dc2b7a97faa556e0989635683c9ad35522fb3ef2
Bil Bas (Spooner) <bil.bagpuss@gmail.com>
2011-12-03 23:23:59 +0000

    Added short preview for blog list.

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -4,4 +4,5 @@ include Nanoc3::Helpers::Breadcrumbs
 include Nanoc3::Helpers::LinkTo
 include Nanoc3::Helpers::Rendering
 include Nanoc3::Helpers::Tagging
+include Nanoc3::Helpers::Text
 include Nanoc3::Helpers::XMLSitemap

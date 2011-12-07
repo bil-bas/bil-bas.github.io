ed2a8e221b9c826e83bfca8dd8b18dee822adac1
Bil Bas (Spooner) <bil.bagpuss@gmail.com>
2011-12-07 22:13:55 +0000

    Fixed link_to_unless_current when passed a block.

--- lib/default.rb
+++ lib/default.rb
@@ -103,7 +103,7 @@ end
 def link_to_unless_current(*args, &block)
   if block_given?
     raise ArgumentError, "Block version requires: path_or_rep, [attributes]" unless (1..2).include? args.size
-    link_to capture(&block), *args
+    link_to_unless_current capture(&block), *args
 
   else
     raise ArgumentError, "Non-block version requires: text, path_or_rep, [attributes]" unless (2..3).include? args.size
